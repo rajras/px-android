8d68ff5ca808043922f8725c4cc6aecb84d88eb7
Jorge <jorge.gonzalez@mercadolibre.com>
2020-05-14 15:09:06 -0300

    remove import

--- px-checkout/src/main/java/com/mercadopago/android/px/internal/features/SecurityCodeActivity.java
+++ px-checkout/src/main/java/com/mercadopago/android/px/internal/features/SecurityCodeActivity.java
@@ -12,7 +12,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.AppCompatButton;
-import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.Toolbar;
 import android.text.InputFilter;
 import android.text.SpannableStringBuilder;
