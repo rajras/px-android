f7789da0af4ac52d82ab1b8b1466326b056d6c81
Cristian Gaggino <cgaggino@gmail.com>
2020-08-20 19:10:14 -0300

    * Added on laid out listener as extension function. * Added set height as extension function * Fixed text view value in bottom description

--- px-checkout/src/main/java/com/mercadopago/android/px/internal/extensions/BaseExtensions.kt
+++ px-checkout/src/main/java/com/mercadopago/android/px/internal/extensions/BaseExtensions.kt
@@ -3,10 +3,11 @@ package com.mercadopago.android.px.internal.extensions
 import android.app.Activity
 import android.graphics.Rect
 import android.view.View
+import androidx.core.view.ViewCompat
 
 internal fun CharSequence?.isNotNullNorEmpty() = !isNullOrEmpty()
 
-internal fun <T : CharSequence>T?.orIfEmpty(fallback: T) = if (isNotNullNorEmpty()) this!! else fallback
+internal fun <T : CharSequence> T?.orIfEmpty(fallback: T) = if (isNotNullNorEmpty()) this!! else fallback
 
 internal fun View.gone() = apply { visibility = View.GONE }
 
@@ -14,13 +15,13 @@ internal fun View.visible() = apply { visibility = View.VISIBLE }
 
 internal fun View.invisible() = apply { visibility = View.INVISIBLE }
 
-internal fun Any?.runIfNull(action: ()->Unit) {
-    if(this == null) {
+internal fun Any?.runIfNull(action: () -> Unit) {
+    if (this == null) {
         action.invoke()
     }
 }
 
-internal fun <T : CharSequence>T?.runIfNotNullNorEmpty(action: (T) -> Unit): Boolean {
+internal fun <T : CharSequence> T?.runIfNotNullNorEmpty(action: (T) -> Unit): Boolean {
     if (isNotNullNorEmpty()) {
         action.invoke(this!!)
         return true
@@ -45,5 +46,22 @@ internal fun Activity.addKeyBoardListener(
                 onKeyBoardClose?.invoke()
             }
         }
+    }
+}
+
+internal fun View?.addOnLaidOutListener(onLaidOut: ((view: View) -> Unit)) {
+    this?.let {
+        if (ViewCompat.isLaidOut(it)) {
+            onLaidOut.invoke(it)
+        }
+        it.addOnLayoutChangeListener { view, _, _, _, _, _, _, _, _ -> onLaidOut.invoke(view) }
+    }
+}
+
+internal fun View?.setHeight(height: Int) {
+    this?.let {
+        val layout = it.layoutParams
+        layout.height = height
+        it.layoutParams = layout
     }
 }
